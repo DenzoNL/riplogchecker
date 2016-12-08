f48c9a90dd94c16648c59911c3a90943a1503bfe
Denzo <denzonl@gmail.com>
2016-12-08 14:44:15 +0100

    # Fixed copy paste error

--- tests/ID3TagsAddedTest.php
+++ tests/ID3TagsAddedTest.php
@@ -8,7 +8,7 @@ use RipLogChecker\RipLogChecker;
 
 class ID3TagsAddedTest extends TestCase
 {
-    public function testC2PointersUsedCheck()
+    public function testID3TagsAdded()
     {
         /* Load log file */
         $testLog = file_get_contents('tests/logs/id3_tags_test.log');
@@ -19,7 +19,7 @@ class ID3TagsAddedTest extends TestCase
         /* Retrieve the errors array */
         $errors = $log_checker->getParser()->getErrors();
 
-        /* Assert that we get the C2_POINTERS_USED error */
+        /* Assert that we get the ID3_TAGS_ADDED error */
         $this->assertEquals($errors[EacParser::ID3_TAGS_ADDED], true);
 
         /* Verify that the score is equals the score that a log with only this error would have */
